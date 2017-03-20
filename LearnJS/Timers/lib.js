0131bead49b824470018df7d09af487a290c542e
Nostradame <eagl@bmail.ru>
2017-03-20 23:25:06 +0300

    setTimeout и setInterval, часть 1.

---
+++ LearnJS/Timers/lib.js
@@ -0,0 +1,33 @@
+﻿function printNumberInterval() {
+	var i = 0;
+	var timerId = setInterval(function () {
+		console.log(++i);
+		if (i == 20) {
+			clearInterval(timerId);
+			return;
+		}
+		},
+		100);
+};
+
+function printNumberIntervalByTimeout() {
+	var i = 0;
+	var timerId = setTimeout(function Timer() {
+		console.log(++i);
+		if (i != 20) {
+			timerId = setTimeout(Timer, 100);
+		}
+	},
+		100);
+};
+
+function delay(func, ms) {
+	return function () {
+		var context = this;
+		var args = arguments;
+		setTimeout(function () {
+			func.apply(context, args);
+			},
+			ms);
+	}
+};
