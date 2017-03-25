bf4fa42c1be3c30a6d5c7ae2fc38bbe04a8c976d
Nostradame <eagl@bmail.ru>
2017-03-25 15:20:19 +0300

    Наследование классов в JavaScript, часть 3.

---
+++ LearnJS/Class-Inheritance/animating-menu.js
@@ -0,0 +1,24 @@
+﻿function AnimatingMenu(state) {
+	Menu.call(this, state);
+}
+
+AnimatingMenu.prototype = Object.create(Menu.prototype);
+AnimatingMenu.prototype.constructor = AnimatingMenu;
+
+AnimatingMenu.STATE_ANIMATING = 2;
+
+AnimatingMenu.prototype.open = function() {
+	this._state = AnimatingMenu.STATE_ANIMATING;
+	this._timer = setTimeout(Menu.prototype.open.bind(this), 1000);
+}
+
+AnimatingMenu.prototype.close = function() {
+	clearTimeout(this._timer);
+	Menu.prototype.close.call(this);
+}
+
+AnimatingMenu.prototype._stateAsString = function() {
+	if (this._state == AnimatingMenu.STATE_ANIMATING)
+		return "анимация";
+	return Menu.prototype._stateAsString.call(this);
+}
