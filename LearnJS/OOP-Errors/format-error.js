160cf92a130363f8d572cc105db0b01f10f566ce
Nostradame <eagl@bmail.ru>
2017-03-25 17:07:02 +0300

    Свои ошибки, наследование от Error.

---
+++ LearnJS/OOP-Errors/format-error.js
@@ -0,0 +1,15 @@
+﻿function FormatError(message) {
+	SyntaxError.call(this, message);
+
+	this.name = "FormatError";
+	this.message = message;
+
+	if (Error.captureStackTrace) {
+		Error.captureStackTrace(this, this.constructor);
+	} else {
+		this.stack = (new Error()).stack;
+	}
+}
+
+FormatError.prototype = Object.create(SyntaxError.prototype);
+FormatError.prototype.constructor = FormatError;
