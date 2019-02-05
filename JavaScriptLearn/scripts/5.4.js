472b7308f4a90ed38c33f8c128443b3c647dd58b
Denis Vostrikov <eagl@bmail.ru>
2019-02-05 23:10:20 +0300

    Замыкание 2

---
+++ JavaScriptLearn/scripts/5.4.js
@@ -0,0 +1,40 @@
+﻿mathfun = (function () {
+    debugger;
+    
+    return {
+        sum: function() {
+            var result = 0;
+            var sumf = function(operand) {
+                result += operand;
+                return sumf;
+            };
+            var resultf = function() {
+                return result;
+            }
+            var resetf = function() {
+                result = 0;
+            }
+
+            return {
+                sum: sumf,
+                result: resultf,
+                reset: resetf
+            };
+        },
+        buffer: function() {
+            var buffer = "";
+            var bufferf = function (str) {
+                if (arguments.length === 0) {
+                    return buffer;
+                }
+
+                buffer += str.toString();
+            };
+
+            bufferf.clear = function() {
+                buffer = "";
+            }
+            return bufferf;
+        }
+    }
+})();
